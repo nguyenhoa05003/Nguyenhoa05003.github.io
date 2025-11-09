2219080f62e8ed63df21bb922843a290562c30de
Nguyenhoa05003 <Nguyenhoa05003@gmail.com>
2025-11-09 20:56:16 +0700

    Cập nhật nội dung cho main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#nut-an-hien').click(function() {
+        $('#noi-dung-bi-an').toggle(500); 
+    });
+    $('#nut-fade').click(function() {
+        $('#noi-dung-bi-an').fadeToggle(1000);
+    });
+});
